1dfcb7b8c4ea5b53fbecebc89127270ef3104b0e
FixonTech <146366763+FixonTech@users.noreply.github.com>
2024-09-25 18:01:00 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const content = document.querySelector('.content');
-
-    const x = (e.pageX - window.innerWidth / 2) / 20;
-    const y = (e.pageY - window.innerHeight / 2) / 20;
-
-    content.style.transform = `translate(-50%, -50%) translate(${x}px, ${y}px) rotateY(${x / 2}deg) rotateX(${y / 2}deg)`;
-});
